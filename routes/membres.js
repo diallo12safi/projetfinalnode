1e079dd9ee014ba00a5e7b3c18fe56c6441afb9b
mamoudou-git <baldegit2302@gmail.com>
2025-08-14 17:20:40 +0200

    mise a jour liste membres

--- routes/membres.js
+++ routes/membres.js
@@ -4,10 +4,12 @@ import { authenticateJWT, isAdmin } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-// Toutes les routes sont protégées par JWT et réservées à l'admin
-router.use(authenticateJWT, isAdmin);
 
+// Route GET accessible sans protection
 router.get('/', getMembres); // Liste des membres
+
+// Les autres routes sont protégées par JWT et réservées à l'admin
+router.use(authenticateJWT, isAdmin);
 router.post('/', addMembre); // Ajouter un membre
 router.put('/:id', updateMembre); // Modifier un membre
 router.delete('/:id', deleteMembre); // Supprimer un membre
